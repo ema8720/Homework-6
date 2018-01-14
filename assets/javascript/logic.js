ba245fb3712c335b6661686ab4db9482361d9c82
Juan Correa <ema8720@gmail.com>
2018-01-13 20:32:56 -0800

    added basic styling

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,37 +1,99 @@
-$(function() {
-    populateButtons(searchArray, 'searchButton', '#buttonsArea');
-    console.log("Page Loaded!");
-})
+var topics = ["Italian Greyhound", "Pug", "French Bulldog", "Great Dane", "Island"];
 
-var searchArray = ["Dog", "Cat", "birds", "music", ];
+var url = "https://api.giphy.com/v1/";
+var apiKey = "9pthiJFYkqUtsmNc6fCiSJJPSGr8fD2S&limit=10";
+var search = "";
+var queryURL = "";
 
-function populateButtons(searchArray, classToAdd, areaToAddTo) {
-    $(areaToAddTo).empty();
-    for (var i = 0; i < searchArray.length; i++) {
-        var a = $('<button>');
-        a.addClass(classToAdd);
-        a.attr('data-type', searchArray[i])
-        a.text(searchArray[i]);
-        $(areaToAddTo).append(a);
+// Creating a button 
+function createButton() {
+    $("#button").empty();
+    for (var i = 0; i < topics.length; i++) {
+        var buttonHTML = "<button class='button'>" + topics[i] + "</button>"
+        $("#button").append(buttonHTML);
     }
-}
-$(document).on('click', '.searchButton', function() {
-    var type = $(this).data('type');
-    console.log(type);
-    var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + type + '&api_keydc6zaTOxFJmzC&limit=15'
-    $.ajax({ url: queryURL, method: 'GET' })
-        .done(function(response) {
-            console.log(response);
-            for (var i = 0; i < response.data.legth; i++) {
-                var searchDiv = $('<div class= "search-item">');
-                var rating = response.data[i].rating;
-                var p = $('<p>').text('Rating: ' + rating);
-                var animated = response.data[i].images.fixed_height.url;
-                var still = response.data[i].images.fixed_height_still.url;
-                var image = $('<img>');
-                image.attr('src', still);
-                image.attr('data-still', still);
-                image.attr('data-animated', animated);
-            }
+};
+
+// image creator
+function makeImage() {
+    $(".imageArea").empty();
+    $.ajax({
+            url: queryURL,
+            method: "GET"
         })
-})
+        .done(function(response) {
+            var results = response.data;
+            console.log(results);
+
+
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                    var rating = results[i].rating;
+                    var p = $("<p>").text("Rating: " + rating);
+                    var image = $("<img class='resultGif'>");
+                    var image = $("<img class='resultGif' src='' data-still='" + results[i].images.fixed_height_still.url + "' data-animate='" + results[i].images.fixed_height.url + "'data-state='still'>");
+
+                    var position = i + 3;
+                    if (position % 3 === 0) {
+                        image.attr("src", results[i].images.fixed_height_still.url);
+                        $("#imageLineOne").append(image);
+                        $("#imageLineOne").append(p);
+                    }
+                    if (position % 3 === 1) {
+                        image.attr("src", results[i].images.fixed_height_still.url);
+                        $("#imageLineTwo").append(image);
+                        $("#imageLineTwo").append(p);
+                    }
+                    if (position % 3 === 2) {
+                        image.attr("src", results[i].images.fixed_height_still.url);
+                        $("#imageLineThree").append(image);
+                        $("#imageLineThree").append(p);
+                    }
+                }
+            }
+            animation();
+        });
+    button();
+};
+
+$("#button").empty();
+createButton();
+button();
+
+$("#addSearch").on("click", function(event) {
+    event.preventDefault();
+    search = $("#searchInput").val();
+    console.log(search);
+    topics.push(search);
+    createButton();
+    queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey;
+    makeImage();
+    $("#searchInput").val("");
+});
+
+
+// Create Images
+function button() {
+    $(".button").on("click", function() {
+        // $("#imageHolder").empty();
+        search = $(this).html();
+        // makeImage();
+        queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey;
+        makeImage();
+    });
+}
+
+// Animation function
+function animation() {
+    $(".resultGif").on("click", function() {
+        console.log(this);
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
+}
